4433374bd5a3577c9f9668cf06f59d3af7a533f4
Ctrl-Monish <monishmakkar.mm@gmail.com>
2021-04-17 09:12:25 +0530

    Make new file in app directory in Kotlin Android and be able to write and read the file.

---
+++ app/src/main/java/com/projects/filereadwritedemokotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.projects.filereadwritedemokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonWrite.setOnClickListener {
+            val dataDir = ContextCompat.getDataDir(this)
+            val myFile = File(dataDir,"file.txt")
+            myFile.writeText(editText.text.toString())
+        }
+
+        buttonRead.setOnClickListener {
+            val dataDir = ContextCompat.getDataDir(this)
+            val myFile = File(dataDir,"file.txt")
+            textView.text = myFile.readText()
+        }
+
+    }
+}
